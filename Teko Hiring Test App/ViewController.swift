be5a307b36a6b5b5e560be6db0b3d5bd8770914d
Bui Van Vi <buivankhang9@gmail.com>
2021-08-07 15:40:39 +0700

    Initial Commit

---
+++ Teko Hiring Test App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Teko Hiring Test App
+//
+//  Created by Bui Van Vi on 8/7/21.
+//  Copyright © 2021 Bui Van Vi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
